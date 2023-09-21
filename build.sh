df5a88de4fa5e88c7f48db147577f51efbf18501
Ganesh14389 <68822653+Ganesh14389@users.noreply.github.com>
2023-09-21 16:10:28 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 git clone https://github.com/hkhcoder/vprofile-project.git
-git checkout main
+git checkout $(BRANCH)
 JAVA_HOME="/usr/lib/jvm/java-1.11.0-openjdk-amd64"
 cd /var/lib/jenkins/workspace/build/vprofile-project
 mvn clean install
